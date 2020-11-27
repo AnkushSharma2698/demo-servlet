28cba0a0f6c39af12b404719637394c8b200533b
AnkushSharma2698 <ankush1@ualberta.ca>
2020-11-27 15:14:31 -0700

    now have a working example of entity managements in the demo-servlet application, will build it from scratch in eclipse and test my snippets

--- src/main/java/io/openliberty/sample/system/resources/EventResource.java
+++ src/main/java/io/openliberty/sample/system/resources/EventResource.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import io.openliberty.sample.system.models.Event;
+import jakarta.annotation.Resource;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.json.Json;
 import jakarta.json.JsonArrayBuilder;
@@ -15,10 +16,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
+import jakarta.transaction.UserTransaction;
 
 @RequestScoped
 @WebServlet(name = "events", urlPatterns = { "/events" })
 public class EventResource extends HttpServlet {
+
+    @Resource
+    UserTransaction utx;
 
     @PersistenceContext(name = "jpa-unit")
     // end::PersistenceContext[]
@@ -27,7 +33,14 @@ public class EventResource extends HttpServlet {
     // tag::createEvent[]
     public void createEvent(Event event) {
         // tag::Persist[]
-        em.persist(event);
+        try {
+            utx.begin();
+            em.persist(event);
+            utx.commit();
+        } catch (Exception e) {
+            System.out.println("Exception in create: " + e.getMessage());
+        }
+        
         // end::Persist[]
     }
     // end::createEvent[]
@@ -43,7 +56,13 @@ public class EventResource extends HttpServlet {
     // tag::updateEvent[]
     public void updateEvent(Event event) {
         // tag::Merge[]
-        em.merge(event);
+        try {
+            utx.begin();
+            em.merge(event);
+            utx.commit();
+        } catch (Exception e) {
+            System.out.println("Exception in update: " + e.getMessage());
+        }
         // end::Merge[]
     }
     // end::updateEvent[]
@@ -51,8 +70,15 @@ public class EventResource extends HttpServlet {
     // tag::deleteEvent[]
     public void deleteEvent(Event event) {
         // tag::Remove[]
-        em.remove(event);
         // end::Remove[]
+        try {
+            utx.begin();
+            Event merged = em.merge(event);
+            em.remove(merged);
+            utx.commit();
+        } catch (Exception e) {
+            System.out.println("Exception in delete: " + e.getMessage());
+        }
     }
     // end::deleteEvent[]
 
@@ -69,18 +95,96 @@ public class EventResource extends HttpServlet {
     }
 
     @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        String name = request.getParameter("name");
+        String location = request.getParameter("location");
+        String time = request.getParameter("time");
+        Event prevEvent = readEvent(Integer.parseInt(id));
+        try {
+            if(prevEvent == null) {
+                response.getWriter().println("Could not find the event");
+            } else if(!findEvent(name, location, time).isEmpty()){
+                response.getWriter().println("Could not find the event");
+            } else {
+                prevEvent.setName(name);
+                prevEvent.setLocation(location);
+                prevEvent.setTime(time);
+        
+                updateEvent(prevEvent);
+                response.getWriter().println("Successfully updated event");
+            }
+        } catch(Exception e) {
+            response.getWriter().println("Failed to update event.");
+        }
+        
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Integer id  = Integer.parseInt(request.getParameter("id"));
+            Event event = readEvent(id);
+            if(event == null) {
+                response.getWriter().println("Failed to find event to delete");
+            } else {
+                deleteEvent(event);
+                response.getWriter().println("Successfully deleted event");
+            }
+
+        } catch (Exception e) {
+            response.getWriter().println("Failed to delete Event:" + e.getMessage());
+        }
+        
+    }
+
+    @Override // Create an event
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        // Add a new event
+        String name = request.getParameter("name");
+        String location = request.getParameter("location");
+        String time = request.getParameter("time");
+        Event newEvent = new Event(name, location, time);
+        try {
+            if(!findEvent(name, location, time).isEmpty()) {
+                response.getWriter().println("Event already exists");
+
+            } else {
+                createEvent(newEvent);
+                response.setContentType("text/html;charset=UTF-8");
+                response.getWriter().println("Created Event: " + newEvent.toString());
+            }
+        } catch (Exception e) {
+            System.out.println("Exception" + e.getMessage());
+        }
+        
+    }
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        JsonObjectBuilder builder = Json.createObjectBuilder();
-        JsonArrayBuilder finalArray = Json.createArrayBuilder();
-        for (Event event : readAllEvents()) {
-            builder.add("name", event.getName()).add("time", event.getTime()).add("location", event.getLocation())
-                    .add("id", event.getId());
-            finalArray.add(builder.build());
-        }
-        response.setContentType("text/html;charset=UTF-8");
-
-        response.getWriter().println(finalArray.build().toString());
+        String id = request.getParameter("id");
+        if (id != null) {
+            JsonObjectBuilder builder = Json.createObjectBuilder();
+            Event event = readEvent(Integer.parseInt(id));
+            if(event != null) {
+                builder.add("name", event.getName()).add("time", event.getTime())
+                    .add("location", event.getLocation()).add("id", event.getId());
+            }
+            response.getWriter().println(builder.build().toString());
+        } else {
+            JsonObjectBuilder builder = Json.createObjectBuilder();
+            JsonArrayBuilder finalArray = Json.createArrayBuilder();
+            for (Event event : readAllEvents()) {
+                builder.add("name", event.getName()).add("time", event.getTime()).add("location", event.getLocation())
+                        .add("id", event.getId());
+                finalArray.add(builder.build());
+            }
+            response.setContentType("text/html;charset=UTF-8");
+    
+            response.getWriter().println(finalArray.build().toString());
+        }  
     }
+
 
 }
